9947364f563644f456443c7185e1463920c1c0d6
Leo-prog123 <leonidkasinskuj@gmail.com>
2025-05-08 23:22:02 +0400

    fully done

--- forms/works.py
+++ forms/works.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
+from flask_wtf.file import FileField, FileRequired  # <--------------
 from wtforms import BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class WorksForm(FlaskForm):
     time = StringField('Дата/Время', validators=[DataRequired()])
+    photo = FileField(validators=[FileRequired()])   # <-------------
     address = StringField('Адрес', validators=[DataRequired()])
     mph = IntegerField("Почасовая ставка")
     min_pay = IntegerField("Часы минимальной оплаты")
